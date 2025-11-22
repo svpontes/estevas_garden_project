44fcafa0141487c75b9d533d2a4e99b83f993096
svpontes <svpontes2015@gmail.com>
2025-11-22 00:12:23 +0000

    styles and html register form

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function (req, res, next) {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // disponibiliza o usuário para as rotas seguras
+        req.user = decoded; 
         next();
     } catch (err) {
         return res.status(403).json({ error: "Invalid or expired token." });
